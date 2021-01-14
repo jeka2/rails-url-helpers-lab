dcec05458e92b2b46c7e43a044a2f0986634d117
Yevgeniy <anigeka@gmail.com>
2021-01-14 18:08:24 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,18 @@
 class StudentsController < ApplicationController
-  before_action :set_student, only: :show
+  before_action :set_student, only: [:show, :active]
   
   def index
     @students = Student.all
   end
 
   def show
+    @active_status = @student.active ? "active" : "inactive"
+  end
+
+  def active 
+    @student.active = !@student.active
+    @student.save!
+    redirect_to action: 'show', id: params[:id]
   end
 
   private
